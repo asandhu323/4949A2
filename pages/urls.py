4affe958c64a543ec730e2ece6629d3ac8248208
asandhu323 <78705796+asandhu323@users.noreply.github.com>
2023-04-06 21:07:19 -0700

    Delete pages directory

--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import homePageView, results, homePost
-
-urlpatterns = [
-    path('', homePageView, name='home'),
-    path('homePost/', homePost, name='homePost'),
-    path('<int:education>/<int:self_emp>/<int:app_inc>/<int:co_inc>/<int:prop>/results/', results, name='results'),
-]
